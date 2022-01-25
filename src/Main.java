68cc0d8057ba94057325785a34e7ff173e975dad
lierschied <spielejannis@web.de>
2022-01-25 13:52:10 +0100

    added loading files from a directory

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,15 +17,31 @@ public class Main {
     private static final Pattern sensorIdPattern = Pattern.compile(SENSOR_ID);
 
     public static void main(String[] args) throws IOException {
-        parseText();
+        Scanner s = new Scanner(System.in);
+        System.out.println(args.length);
+        String dirPath;
+        if (args.length > 0) {
+            dirPath = args[0];
+        } else {
+            System.out.print("Specify directory to parse files from: ");
+            dirPath = s.nextLine();
+        }
+
+        File[] files = loadFiles(dirPath);
+
+        for (File f : files) {
+            System.out.println(f.getName());
+            parseText(f.getAbsolutePath());
+        }
     }
 
     /**
      * parsing the opc_output data for sensor values
+     *
      * @throws IOException if file fails to open
      */
-    private static void parseText() throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader("src/opc_output_rl.t"))) {
+    private static void parseText(String fileName) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             while (true) {
                 String line = reader.readLine();
                 if (line == null)
@@ -48,8 +66,18 @@ public class Main {
 
     }
 
+
+    public static File[] loadFiles(String dirPath) {
+        File directoryPath = new File(dirPath);
+        //List of all files and directories
+        File[] filesList = directoryPath.listFiles();
+        assert filesList != null;
+        System.out.println(String.format("Loaded %s file(s) from %s.", filesList.length, directoryPath));
+        return filesList;
+    }
+
     /**
-     * @param p compiled regex Pattern
+     * @param p       compiled regex Pattern
      * @param toMatch string to match the pattern against
      * @return first captured group from a regex
      */
